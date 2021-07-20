19b2f4747f51ff2bbe2f10858b2192211043df39
Gilblasse <nethelbert.blasse@gmail.com>
2021-07-20 19:32:33 -0400

    3 files modified

--- src/App.js
+++ src/App.js
@@ -11,16 +11,28 @@ export default function App() {
   };
 
   return (
-    <div>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <UserAvatar /> <p style={{ margin: 0, paddingLeft: 5 }}>{user.name}</p>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: 25
+      }}
+    >
+      <div style={{ display: 'flex', alignItems: 'center', marginBottom: 35 }}>
+        <UserAvatar />{' '}
+        <p
+          style={{ margin: 0, paddingLeft: 5, fontSize: 14, color: '#6b6b6b' }}
+        >
+          {user.name}
+        </p>
       </div>
 
-      <Row gutter={[20, 20]} style={{ width: 250 }} justify="center">
+      <Row gutter={[40, 35]} style={{ width: 400 }} justify="center">
         {phoneNums.map(num => (
           <Col key={num}>
             <Avatar
-              size={43}
+              size={63}
               onClick={handleClick}
               className="dial"
               style={{ cursor: 'pointer' }}
--- src/UserAvatar.js
+++ src/UserAvatar.js
@@ -3,16 +3,16 @@ import { Avatar } from 'antd';
 import { user } from './data';
 import './style.css';
 
-export default function UserAvatar() {
+export default function UserAvatar({ size }) {
   const [firstName, lastName] = user.name.split(' ');
   const userInitial = `${firstName[0]}${lastName[0]}`;
 
   return (
     <>
       {user.userImgUrl ? (
-        <Avatar src={user.userImgUrl} />
+        <Avatar src={user.userImgUrl} size={size} />
       ) : (
-        <Avatar>{userInitial}</Avatar>
+        <Avatar size={size}>{userInitial}</Avatar>
       )}
     </>
   );
